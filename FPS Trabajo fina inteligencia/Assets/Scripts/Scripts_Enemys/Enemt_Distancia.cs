df0c7db67b3658f6f51996ce2c3415747e898152
ldavidmf123mf <38573782+ldavidmf123mf@users.noreply.github.com>
2018-12-02 21:23:40 -0600

    avance de trabajo final
    se realizo la implementacion de las mecanicas del jugador asi como el enemigo cuerpo a cuerpo con el enemigo de distancia

--- FPS Trabajo fina inteligencia/Assets/Scripts/Scripts_Enemys/Enemt_Distancia.cs
+++ FPS Trabajo fina inteligencia/Assets/Scripts/Scripts_Enemys/Enemt_Distancia.cs
@@ -2,36 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
-public class Enemy_CuerpoCuerpo : Base_Enemys
+public class Enemt_Distancia : Base_Enemys
 {
-    public enum EstadoMaquina { estatico, patrullaje, persecucion };
+    public enum EstadoMaquina { estatico, patrullaje, detectar, ataque };
     public EstadoMaquina estadoActual;
-    public float VidaEnemyCuerpo = 100f;
     public Camera gameCamara;
     NavMeshAgent agente; // NPC
     int numWaypoint = 0;
-    public float auxVelocidad;
+    float auxVelocidad;
     public float distancia;
     public float tiempoEspera;
+    public float iniciobala;
+    public float esperabala;
+    public float VidaEnemyDistancia = 100f;
+    public Transform balaposicion;
 
     // Use this for initialization
-   protected override void Start()
+    protected override void Start()
     {
         base.Start();
         agente = GetComponent<NavMeshAgent>();
         auxVelocidad = agente.speed;
+        InvokeRepeating("EstadoDetectar", iniciobala, esperabala);
+
     }
 
+    // Update is called once per frame
     void Update()
     {
         Estadoestatico();
         Estadopatrullaje();
-        Estadopersecucion();
+        EstadoDetectar();
     }
 
-    // Update is called once per frame
     protected override void Estadoestatico()
     {
         if (estadoActual == EstadoMaquina.estatico)
@@ -44,6 +48,7 @@ public class Enemy_CuerpoCuerpo : Base_Enemys
             }
         }
     }
+
     protected override void Estadopatrullaje()
     {
         if (estadoActual == EstadoMaquina.patrullaje)
@@ -57,36 +62,47 @@ public class Enemy_CuerpoCuerpo : Base_Enemys
         }
     }
 
-    protected override void Estadopersecucion()
+    protected void EstadoDetectar()
     {
-        if (Player != null && Vector3.Distance(transform.position, Player.transform.position) <= distanciaTarget)
+        if (estadoActual == EstadoMaquina.detectar)
         {
-            agente.isStopped = false;
-            agente.destination = Player.transform.position;
-            if (agente.remainingDistance <= agente.stoppingDistance && !agente.pathPending)
-            {
-                agente.isStopped = true;
 
+            if (Vector3.Distance(transform.position, agente.transform.position) < distancia)
+            {
+                agente.stoppingDistance = 3f;
+                transform.LookAt(Player.transform.position);
+                Instantiate(Bala, balaposicion.position, balaposicion.rotation);
             }
         }
     }
 
-
-void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag=="Bullet")
+        if (other.tag == "Player")
+        {
+            Debug.Log("colision");
+            Player = other.gameObject;
+            estadoActual = EstadoMaquina.detectar;
+            agente.isStopped = true;
+        }
+        if (other.tag == "Bullet")
         {
             Debug.Log("pego");
-            VidaEnemyCuerpo = VidaEnemyCuerpo - 10;
+            VidaEnemyDistancia = VidaEnemyDistancia - 10;
         }
         if (other.tag == "BulletEscopeta")
         {
             Debug.Log("pegoEscopeta");
-            VidaEnemyCuerpo = VidaEnemyCuerpo - 15;
+            VidaEnemyDistancia = VidaEnemyDistancia - 10;
         }
-        if (VidaEnemyCuerpo == 0)
+        if (VidaEnemyDistancia == 0)
         {
             Destroy(this.gameObject);
         }
+    }
+
+    protected override void Estadopersecucion()
+    {
+        throw new System.NotImplementedException();
     }
 }

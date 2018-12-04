564d100695f9a9545ad833aeaebffb63d41fb85d
IAGER515 <38573804+IAGER515@users.noreply.github.com>
2018-12-04 15:10:22 -0600

    Enemigo_Boss
    Se crea una nueva escena en donde se encontrar el boss
    Se crea su script asi como una parte de la segunda fase de vida

---
+++ FPS Trabajo fina inteligencia/Assets/Scripts/Scripts_Enemys/Enemy_Boss.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_Boss : Base_Enemys {
+
+    public float VidaBoss = 1000;
+    public GameObject Escudo;
+    NavMeshAgent Agent;
+
+
+
+
+
+    protected override void Estadoestatico()
+    {
+        if (VidaBoss >= 500)
+        {
+            Escudo.SetActive(false);
+        }
+    }
+    public void Rage()
+    {
+        if (VidaBoss < 500)
+        {
+            Escudo.SetActive(true);
+        }
+    }
+    protected override void Estadopatrullaje()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Estadopersecucion()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("colision");
+            Player = other.gameObject;
+            
+       
+          
+        }
+        if (other.tag == "Bullet")
+        {
+            Debug.Log("pego");
+            VidaBoss -=  10;
+        }
+        if (other.tag == "BulletEscopeta")
+        {
+            Debug.Log("pegoEscopeta");
+            VidaBoss -= 10;
+        }
+        if (VidaBoss == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
